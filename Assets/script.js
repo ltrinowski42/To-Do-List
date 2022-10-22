90ccf26112118522f0d745986d9fd5f480bd0d0e
ltrinowski <ltrinowski42@gmail.com>
2022-10-22 18:24:20 -0400

    started adding arrays

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,28 @@
 //select elements
 const form = document.getElemebtById("todoform")
+const todoInput = document.getElemebtById("newtodo")
+
+//variables
+let todos = [];
 
 //form submit
-form.addEventListener('submit', function () {
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
 
-
+    saveTodo();
 })
+
+//save todo
+function saveToDo(){
+    const todoValue = todoInput.value
+
+    const todo = {
+       value : todoValue,
+       checked :false,
+       color : '#' + Math.floor(Math.random() * 16777215).toString(16),
+    }
+
+    todos.push(todo);
+
+    console.log(todos);
+}
